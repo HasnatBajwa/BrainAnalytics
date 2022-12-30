0092c0f4181769aacf6f4085da39d9d7cde2107a
hasnatbajwa <hasnatjbajwa@gmail.com>
2022-12-30 13:18:39 +0500

    Mobile responsive issues resolved

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./FooterStyles.css";
+import { Grid } from "@material-ui/core";
+
+const Footer = () => {
+  return(
+<div className="footer-container">
+    <Grid container>
+        <Grid item>
+
+        </Grid>
+    </Grid>
+</div>
+  );
+};
+
+export default Footer;
