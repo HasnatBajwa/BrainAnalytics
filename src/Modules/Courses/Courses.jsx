68d5ce505525ae569d379673478aac41c4c737f6
Hasnat <hasnatjbajwa@gmail.com>
2023-01-14 17:12:38 +0500

    updated courses cards

--- src/Modules/Courses/Courses.jsx
+++ src/Modules/Courses/Courses.jsx
@@ -1,48 +1,62 @@
 import React from "react";
-import "./CoursesStyles.css";
 import { Grid } from "@material-ui/core";
+import "./CoursesStyles.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation } from "swiper";
 import digitalMarketing from '../../assets/Courses/digitalmarketing.png';
 import graphicDesigning from '../../assets/Courses/graphic-designing.png';
 import dataAnalysis from '../../assets/Courses/data-analysis.png';
 import virtualAssitant from '../../assets/Courses/virtualassistant.png';
-import webAndApp from '../../assets/Courses/webandapp.png'; 
+import webAndApp from '../../assets/Courses/webandapp.png';
+import "swiper/swiper.min.css";
 
 const Courses = () => {
   return (
     <div className="courses-container">
       <Grid container>
-        <Grid item xs={12} sm={12} md={12}>
-          <div>
-            <h1 data-aos="fade-right" className="courses-heading">
-              Our Courses
-            </h1>
-          </div>
-        </Grid>
-        <Grid item xs={12} sm={12} md={6}>
-          <div data-aos='fade-right' className="course-card dm">
-            <img src={digitalMarketing} alt="Digital-Marketing" className="course-img" />
-          </div>
-        </Grid>
-        <Grid item xs={12} sm={12} md={6}>
-          <div data-aos='fade-left' className="course-card">
-            <img src={dataAnalysis} alt="Data-Analysis" className="course-img" />
-          </div>
-        </Grid>
-        <Grid item xs={12} sm={12} md={4}>
-          <div data-aos='fade-up' className="course-card">
-            <img src={graphicDesigning} alt="Graphic-Designing" className="course-img" />
-          </div>
-        </Grid>
-        <Grid item xs={12} sm={12} md={4}>
-          <div data-aos='fade-up' className="course-card">
-            <img src={virtualAssitant} alt="Virtual-Assistant" className="course-img" />
-          </div>
-        </Grid>
-        <Grid item xs={12} sm={12} md={4}>
-          <div data-aos='fade-up' className="course-card">
-            <img src={webAndApp} alt="Web-and-App" className="course-img" />
-          </div>
-        </Grid>
+        <Swiper
+        spaceBetween={30}
+        slidesPerView={3}
+        autoplay={{
+          delay: 1500,
+          disableOnInteraction: false,
+          pauseOnMouseEnter:true,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={false}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+        >
+          <SwiperSlide>
+            <div className="course-card">
+              <img src={digitalMarketing} alt="courses" className="course-img" />
+              </div>
+
+          </SwiperSlide>
+          <SwiperSlide>
+
+            <div className="course-card">
+              <img src={graphicDesigning} alt="courses" className="course-img" />
+            </div>
+          </SwiperSlide><SwiperSlide>
+
+            <div className="course-card">
+              <img src={dataAnalysis} alt="courses" className="course-img" />
+            </div>
+          </SwiperSlide><SwiperSlide>
+
+            <div className="course-card">
+              <img src={virtualAssitant} alt="courses" className="course-img" />
+            </div>
+          </SwiperSlide><SwiperSlide>
+
+            <div className="course-card">
+              <img src={webAndApp} alt="courses" className="course-img" />
+            </div>
+          </SwiperSlide>
+        </Swiper>
       </Grid>
     </div>
   );
