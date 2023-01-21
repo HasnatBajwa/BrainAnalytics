3253194a3a8623b6b6a29a59487bb336cfcb2f69
Hasnat <hasnatjbajwa@gmail.com>
2023-01-21 17:35:53 +0500

    update

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -23,7 +23,7 @@ const Services = () => {
                             <img src={gd} alt="grapic" className="service-img" />
                             <h3 className="service-title">Business Card Design</h3>
                             <p className="service-desc">Get your Business Card Designed from our highly skilled and specialized designers</p>
-                            <Link to='/Contact'><button className="get-service">Get Qoute</button></Link>
+                            <Link to='/ContactUs'><button className="get-service">Get Qoute</button></Link>
                         </div>
                     </Grid>
                     <Grid item xs={12} sm={12} md={3}>
@@ -31,7 +31,7 @@ const Services = () => {
                             <img src={gd} alt="grapic" className="service-img" />
                             <h3 className="service-title">Posters Design</h3>
                             <p className="service-desc">Get your Social Media Designed from our highly skilled and specialized designers</p>
-                            <Link to='/Contact'><button className="get-service">Get Qoute</button></Link>
+                            <Link to='/ContactUs'><button className="get-service">Get Qoute</button></Link>
                         </div>
                     </Grid>
                     <Grid item xs={12} sm={12} md={3}>
@@ -39,7 +39,7 @@ const Services = () => {
                             <img src={gd} alt="grapic" className="service-img" />
                             <h3 className="service-title">Logo Design</h3>
                             <p className="service-desc">Get your Company Logo Designed from our highly skilled and specialized designers</p>
-                            <Link to='/Contact'><button className="get-service">Get Qoute</button></Link>
+                            <Link to='/ContactUs'><button className="get-service">Get Qoute</button></Link>
                         </div>
                     </Grid>
                     <Grid item xs={12} sm={12} md={3}>
@@ -47,7 +47,7 @@ const Services = () => {
                             <img src={gd} alt="grapic" className="service-img" />
                             <h3 className="service-title">Banner Design</h3>
                             <p className="service-desc">Get your Advertising Banner Designed from our highly skilled and specialized designers</p>
-                            <Link to='/Contact'><button className="get-service">Get Qoute</button></Link>
+                            <Link to='/ContactUs'><button className="get-service">Get Qoute</button></Link>
                         </div>
                     </Grid>
                 </Grid>
@@ -59,7 +59,7 @@ const Services = () => {
                             <img src={sd}alt="grapic" className="service-img" />
                             <h3 className="service-title">Website Development</h3>
                             <p className="service-desc">Get your Personal or Business website developed from our highly skilled and specialized engineers</p>
-                            <Link to='/Contact'><button className="get-service">Get Qoute</button></Link>
+                            <Link to='/ContactUs'><button className="get-service">Get Qoute</button></Link>
                         </div>
                     </Grid>
                     <Grid item xs={12} sm={12} md={3}>
@@ -67,7 +67,7 @@ const Services = () => {
                             <img src={sd} alt="grapic" className="service-img" />
                             <h3 className="service-title">Mobile Application</h3>
                             <p className="service-desc">Get your IOS & Android App developed from our highly skilled and specialized engineers</p>
-                            <Link to='/Contact'><button className="get-service">Get Qoute</button></Link>
+                            <Link to='/ContactUs'><button className="get-service">Get Qoute</button></Link>
                         </div>
                     </Grid>
                     <Grid item xs={12} sm={12} md={3}>
@@ -75,7 +75,7 @@ const Services = () => {
                             <img src={sd} alt="grapic" className="service-img" />
                             <h3 className="service-title">Database Development</h3>
                             <p className="service-desc">Get your Database developed from our highly skilled and specialized developers</p>
-                            <Link to='/Contact'><button className="get-service">Get Qoute</button></Link>
+                            <Link to='/ContactUs'><button className="get-service">Get Qoute</button></Link>
                         </div>
                     </Grid>
                     <Grid item xs={12} sm={12} md={3}>
@@ -83,7 +83,7 @@ const Services = () => {
                             <img src={sd} alt="grapic" className="service-img" />
                             <h3 className="service-title">Financial Analysis</h3>
                             <p className="service-desc">Get your Financial Analysis Dashboard developed in Power BI from our highly skilled and specialized developers</p>
-                            <Link to='/Contact'><button className="get-service">Get Qoute</button></Link>
+                            <Link to='/ContactUs'><button className="get-service">Get Qoute</button></Link>
                         </div>
                     </Grid>
                 </Grid>
@@ -95,7 +95,7 @@ const Services = () => {
                             <img src={ed}alt="grapic" className="service-img" />
                             <h3 className="service-title">Product Hunting</h3>
                             <p className="service-desc">Get the Product you need to outstand the marketplace</p>
-                            <Link to='/Contact'><button className="get-service">Get Qoute</button></Link>
+                            <Link to='/ContactUs'><button className="get-service">Get Qoute</button></Link>
                         </div>
                     </Grid>
                     <Grid item xs={12} sm={12} md={3}>
@@ -103,7 +103,7 @@ const Services = () => {
                             <img src={ed} alt="grapic" className="service-img" />
                             <h3 className="service-title">listing Optimization</h3>
                             <p className="service-desc">Get your Listing optimized so that your products are displayed on the firstpage</p>
-                            <Link to='/Contact'><button className="get-service">Get Qoute</button></Link>
+                            <Link to='/ContactUs'><button className="get-service">Get Qoute</button></Link>
                         </div>
                     </Grid>
                     <Grid item xs={12} sm={12} md={3}>
@@ -111,7 +111,7 @@ const Services = () => {
                             <img src={ed} alt="grapic" className="service-img" />
                             <h3 className="service-title">Product marketing</h3>
                             <p className="service-desc">Not getting enough orders? Let's get your product marketed so that it reaches the right audience</p>
-                            <Link to='/Contact'><button className="get-service">Get Qoute</button></Link>
+                            <Link to='/ContactUs'><button className="get-service">Get Qoute</button></Link>
                         </div>
                     </Grid>
                     <Grid item xs={12} sm={12} md={3}>
@@ -119,7 +119,7 @@ const Services = () => {
                             <img src={ed} alt="grapic" className="service-img" />
                             <h3 className="service-title">Shopify Dropshipping</h3>
                             <p className="service-desc">Get your Shopify Dropshipping store ready in no time so you can start working on it asap</p>
-                            <Link to='/Contact'><button className="get-service">Get Qoute</button></Link>
+                            <Link to='/ContactUs'><button className="get-service">Get Qoute</button></Link>
                         </div>
                     </Grid>
                 </Grid>
